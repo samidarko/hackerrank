ddd00acdd171c3c3e01e27a11aefaafb71430d89
Vincent Dupont <samidarko@gmail.com>
2018-04-09 11:13:44 +0800

    algorithms/sorting/quicksort1

--- algorithms/sorting/quicksort1.hs
+++ algorithms/sorting/quicksort1.hs
@@ -1,10 +1,13 @@
 -- https://www.hackerrank.com/challenges/quicksort1/problem
+import Data.List (intercalate)
 
--- partition p xs = fn xs
---   where 
---     fn [] = []
---     fn (y:ys)
---       | y < p =
+aggregator v (lt, eq, gt) = let p = head eq
+                             in if v < p then (v:lt, eq, gt)
+                                         else if v > p then (lt, eq, v:gt)
+                                         else (lt, v:eq, gt)
+
+partition xs = let (lt, eq, gt) = foldr aggregator ([], [head xs], []) (tail xs)
+                in (lt ++ eq ++ gt)
 
 main :: IO ()
 main = do
@@ -12,13 +15,13 @@ main = do
     let n = read n_temp :: Int
     arr_temp <- getLine
     let arr = map read $ words arr_temp :: [Int]
-    putStrLn ""
+    putStrLn . intercalate " " .  map show $ partition arr
 
 getMultipleLines :: Int -> IO [String]
 getMultipleLines n
     | n <= 0 = return []
-    | otherwise = do          
-        x <- getLine         
-        xs <- getMultipleLines (n-1)    
-        let ret = (x:xs)    
-        return ret          
+    | otherwise = do
+        x <- getLine
+        xs <- getMultipleLines (n-1)
+        let ret = (x:xs)
+        return ret
