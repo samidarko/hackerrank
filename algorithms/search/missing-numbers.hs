892981d6c75b6f3831fbacf3cb492db360071571
vincent <vincent@gephen.co>
2018-04-16 11:35:08 +0800

    algorithms/search/missing-numbers

--- algorithms/search/missing-numbers.hs
+++ algorithms/search/missing-numbers.hs
@@ -1,5 +1,8 @@
+-- https://www.hackerrank.com/challenges/missing-numbers/problem
 import qualified Data.Map as M
 import qualified Data.Set as S
+import Data.List (sort, intercalate)
+import Data.Maybe (isJust)
 
 createMap xs = foldl (\m v -> M.insertWith (+) v 1 m) M.empty xs
 
@@ -14,11 +17,13 @@ main = do
     brr_temp <- getLine
     let brr = map read $ words brr_temp :: [Int]
     let ma = createMap arr
-    let sa = S.fromList ma
+    let sa = S.fromList $ M.keys ma
     let mb = createMap brr
-    let sb = S.fromList mb
+    let sb = S.fromList $ M.keys mb
+    let helper x = M.lookup x ma >>= (\y -> if y < 2 then Nothing else M.lookup x mb >>= (\z -> if y == z then Nothing else Just x))
     let common = S.intersection sa sb
-    putStrLn ""
+    let result = intercalate " " . map show . sort . S.toList $ S.filter (isJust . helper) common
+    putStrLn result
 
 getMultipleLines :: Int -> IO [String]
 getMultipleLines n
