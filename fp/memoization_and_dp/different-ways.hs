f5d502ab60becc381d2b281cfb9dd07597a4c476
samidarko <samidarko@gmail.com>
2018-04-29 00:31:40 +0200

    fp/memoization_and_dp/different-ways

--- fp/memoization_and_dp/different-ways.hs
+++ fp/memoization_and_dp/different-ways.hs
@@ -1,16 +1,17 @@
 -- https://www.hackerrank.com/challenges/different-ways-fp/problem
 
-fact :: (Enum a, Num a) => a -> a
+fact :: (Enum a, Num a, Eq a) => a -> a
+fact 0 = 1
 fact n = product [1..n]
 
 combinations :: Integral a => a -> a -> a
 combinations n k
   | k == 0 = 1
   | k == n = 1
-  | otherwise = (fact n `div` (fact (n-k) * fact k)) -- `mod` (10^8+7)
+  | otherwise = (fact n `div` (fact (n-k) * fact k)) `mod` (10^8+7)
 
 processLine s = combinations n' k'
-  where toInt x = read x :: Int
+  where toInt x = read x :: Integer
         [n, k] = words s
         n' = toInt n
         k' = toInt k
@@ -20,3 +21,4 @@ main = do
     getLine
     contents <- getContents
     mapM_ (print . processLine) $ lines contents
+
